6077476109af191e0a99c0940f84162f35796ed3
anik4997 <oliahammed02@gmail.com>
2026-02-24 00:24:28 +0600

    Product entries

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all();
+        return view('products',compact('products'));
+    }
+
+    public function store(Request $request)
+    {
+        Product::create($request->all());
+        return back();
+    }
 }
---
+++ resources/views/products.blade.php
@@ -0,0 +1,17 @@
+<h2>Add Product</h2>
+<form method="POST" action="/product/store">
+@csrf
+<input name="name" placeholder="Name">
+<input name="purchase_price" placeholder="Purchase">
+<input name="sell_price" placeholder="Sell">
+<input name="stock" placeholder="Stock">
+<button>Add</button>
+</form>
+
+<h2>Product List</h2>
+@foreach($products as $p)
+<p>{{ $p->name }} | Stock: {{ $p->stock }}</p>
+@endforeach
+
+<a href="/sale/create">Make Sale</a>
+<a href="/report">Report</a>
